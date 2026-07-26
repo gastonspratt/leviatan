74aae278ac7bfaf24529dcd46801158cd5420223
gastonspratt <gastonspratt@outlook.com>
2026-07-26 01:19:56 -0300

    Load and parse CSV from Google Sheets
    Added functionality to load and parse a CSV from a Google Sheets URL using PapaParse.

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,1 +1,24 @@
+const URL_SHEET =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vR_LPxA_j_r4zr2_LJAlf03uqkXrW2uj1dZE-diFxU8TD0ta0uh5_CFFoZdmbPVdCAJfg6dOfyjWVgt/pub?gid=0&single=true&output=csv";
 
+Papa.parse(URL_SHEET, {
+
+    download: true,
+
+    header: true,
+
+    skipEmptyLines: true,
+
+    complete: function(resultado){
+
+        console.clear();
+
+        console.log("Catálogo cargado");
+
+        console.table(resultado.data);
+
+        console.log("Cantidad de discos:", resultado.data.length);
+
+    }
+
+});
