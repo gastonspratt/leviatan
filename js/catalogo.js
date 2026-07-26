cd08a6a7e94c643f75415f9c4ffb18c63000af3c
gastonspratt <gastonspratt@outlook.com>
2026-07-26 10:36:21 -0300

    Update catalogo variable and improve logging
    Refactor catalogo variable assignment and logging.

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,6 +1,8 @@
 const URL_SHEET =
 "https://docs.google.com/spreadsheets/d/e/2PACX-1vR_LPxA_j_r4zr2_LJAlf03uqkXrW2uj1dZE-diFxU8TD0ta0uh5_CFFoZdmbPVdCAJfg6dOfyjWVgt/pub?gid=0&single=true&output=csv";
 
+let catalogo = [];
+
 Papa.parse(URL_SHEET, {
 
     download: true,
@@ -11,14 +13,11 @@ Papa.parse(URL_SHEET, {
 
     complete: function(resultado){
 
-    window.catalogo = resultado.data;
+        catalogo = resultado.data;
 
-    console.table(catalogo);
+        console.log("Cantidad de discos:", catalogo.length);
 
-    console.log("Cantidad de discos:", catalogo.length);
-
-    console.table(Object.keys(catalogo[0]));
-}
+    }
 
 });
 
